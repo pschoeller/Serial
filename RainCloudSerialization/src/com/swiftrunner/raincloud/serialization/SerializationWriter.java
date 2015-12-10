38d2428dafd17076e68b560fa16c84214abec0f8
pschoeller <pschoeller001@gmail.com>
2015-12-09 20:38:10 -0600

    Finished write and added read.

--- RainCloudSerialization/src/com/swiftrunner/raincloud/serialization/SerializationWriter.java
+++ RainCloudSerialization/src/com/swiftrunner/raincloud/serialization/SerializationWriter.java
@@ -6,6 +6,12 @@ public class SerializationWriter{
 	public static final short	VERSION	= 0x0100;
 	
 	
+	public static int writeBytes(byte[] dest, int pointer, byte[] src){
+		for(int i=0; i<src.length; i++){ dest[pointer++] = src[i]; }
+		return pointer;
+	}
+	
+	
 	public static int writeBytes(byte[] dest, int pointer, byte value){ 
 		dest[pointer++] = value;
 		return pointer;
@@ -63,5 +69,53 @@ public class SerializationWriter{
 	public static int writeBytes(byte[] dest, int pointer, boolean value){
 		dest[pointer++] = (byte)(value ? 1 : 0);
 		return pointer;
+	}
+	
+	
+	public static int writeBytes(byte[] dest, int pointer, String string){
+		pointer = writeBytes(dest, pointer, (short) string.length());
+		return writeBytes(dest, pointer, string.getBytes());
+	}
+	
+	
+	public static byte readbyte(byte[] src, int pointer){
+		return src[pointer];
+	}
+	
+	
+	public static char readChar(byte[] src, int pointer){
+		return (char) ((src[pointer] << 8) | (src[pointer + 1]));
+	}
+	
+	
+	public static short readShort(byte[] src, int pointer){
+		return (short) ((src[pointer] << 8) | (src[pointer + 1]));
+	}
+	
+	
+	public static int readInt(byte[] src, int pointer){
+		return (int) ((src[pointer] << 24) | (src[pointer + 1] << 16) | (src[pointer + 2] << 8) | (src[pointer + 3]));
+	}
+	
+	
+	public static long readLong(byte[] src, int pointer){
+		return (long) 	((src[pointer] << 56) | (src[pointer + 1] << 48) | (src[pointer + 2] << 40) | (src[pointer + 3] << 32) | 
+						(src[pointer + 4] << 24) | (src[pointer + 5] << 16) | (src[pointer + 6] << 8) | (src[pointer + 7]));
+	}
+	
+	
+	public static float readFloat(byte[] src, int pointer){
+		return Float.intBitsToFloat(readInt(src, pointer));
+	}
+	
+	
+	public static double readDouble(byte[] src, int pointer){
+		return Double.longBitsToDouble(readLong(src, pointer));
+	}
+	
+	
+	public static boolean readBoolean(byte[] src, int pointer){
+		assert(src[pointer] == 0 || src[pointer] == 1);
+		return src[pointer] != 0;
 	}
 }
